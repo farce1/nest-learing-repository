b9f3594f9c411f94b9046fd44dcc0f21cf78bfc0
Grzegorz Sztuczyński <19194188+farce1@users.noreply.github.com>
2022-06-11 23:16:45 +0200

    chore: add simple return to auth module for test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  // auth/signup
+  @Post('signup')
+  signup() {
+    return 'I am signed up'
+  }
+
+  // auth/signin
+  @Post('signin')
+  signin() {
+    return 'I am signin in'
+  }
 }
