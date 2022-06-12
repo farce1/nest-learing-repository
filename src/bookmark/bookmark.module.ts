d628cb151f4a4190790f733af74a4ca3163b3802
Grzegorz Sztuczyński <19194188+farce1@users.noreply.github.com>
2022-06-13 00:15:14 +0200

    refactor: update code styling

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
-import { BookmarkController } from './bookmark.controller';
-import { BookmarkService } from './bookmark.service';
+import { BookmarkController } from './bookmark.controller'
+import { BookmarkService } from './bookmark.service'
 
 @Module({
   controllers: [BookmarkController],
